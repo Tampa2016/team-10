8aa3f329acd0da4485291028a9416a8316774114
stanleycelestin1 <stanleycelestin1@gmail.com>
2016-02-05 22:06:27 -0500

    Place Class

--- Place.java
+++ Place.java
@@ -1,3 +1,29 @@
 public class Place{
+    String name;
+    int placeID;
+    String address;
+    public Place(String n,int p,String a){
+        name =n;
+        placeID = p;
+        address= a;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String n){
+        name =n;
+    }
+    public String getplaceID(){
+        return String.valueOf(placeID);
+    }
+    public void setplaceID(int id){
+        this.placeID = id;
+    }
+    public String getAddress(){
+        return this.address;
+    }
+    public void setAddress(String a){
+        this.address =a;
+    }
 
 }
