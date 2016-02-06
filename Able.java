16db2ce4d3c26a27ae7c237e5c4b5506f76bbf30
stanleycelestin1 <stanleycelestin1@gmail.com>
2016-02-06 00:22:35 -0500

    Create Able.java

---
+++ Able.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Able {
+public static void main(String [] args){
+	String [] questions = new String[5];
+	questions[0]="On a scale from 1 to 5, how would you rate your overall service?";
+	questions[1]="Were the bathrooms to your satisfaction? 'y' or 'n'";
+	questions[2]="Did you have a hard time getting INTO the establishment? 'y' or 'n'";
+	questions[3]="Did you have a hard time getting service once you were INSIDE the establishment? 'y' or 'n'";
+	questions[4]="Did you have a hard time getting TO the establishment? 'y' or 'n'";
+	
+	ArrayList<String> answers1 = new ArrayList<String>();
+	ArrayList<String> answers2 = new ArrayList<String>();
+	ArrayList<String> answers3 = new ArrayList<String>();
+	ArrayList<String> answers4 = new ArrayList<String>();
+	ArrayList<String> answers5 = new ArrayList<String>();
+}
+public void review(){
+	
+}
+}
